68530db91e743bcf01fa73cd4ebb8bbc98f86bdc
Mario Bastiampillai <mariobastiampillai@outlook.com>
2019-11-09 19:53:08 -0500

    Work on the System Admin page, including HTML and CSS

---
+++ SystemAdmin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>System Admin</title>
+<style>
+body {background-color: lightgreen;
+background-image:linear-gradient(to right, lightgreen, mediumspringgreen);
+}
+#grad1{
+	background-color:powderblue;
+	background-image:linear-gradient(to right, powderblue, mediumspringgreen);
+	width: 300px;
+	height: 600px;
+	border: 3px solid orange;
+	padding-left: 10px;
+}
+.button{
+	background-color: #4CAF50;
+	border: none;
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	margin:4px 2px;
+	cursor: pointer;
+	}
+</style>
+</head>
+<body>
+<h1>
+System Admin 
+<p>Welcome System Admin!</p>
+</h1>
+<hr>
+<div id="grad1">
+<br>
+<button class="button">Add Event</button>
+<br>
+<br>
+<button class="button">Assign Manager</button>
+<br>
+<br>
+<button class="button">Add Participant</button>
+<br>
+<br>
+<button class="button">Change Event Status</button>
+<br>
+<br>
+<button class="button">View List of Events</button>
+<br>
+<br>
+<button class="button">View List of Participants</button>
+<br>
+<br>
+</div>
+</body>
+</html>
