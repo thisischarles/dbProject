7b380eb010eb7c34035f1e02702e94e1ff15549f
Jessica Allaire <jessica.allaire.96@hotmail.com>
2019-11-10 16:54:15 -0500

    Added event admin homepage and group files

---
+++ Event_Admin/Event_Admin_Homepage.php
@@ -0,0 +1,31 @@
+<?php
+include('../System/Header.php');
+?>
+<h1>
+    Welcome to your personal homepage! Click on the options below to perform an action!
+</h1>
+<h2>
+    Modify Event
+</h2>
+<h2>
+    Remove/Add/Update Participants
+</h2>
+<h2>
+    Post Content
+</h2>
+<h2>
+    Upload CSV
+</h2>
+<h2>
+    List of Events
+    <h3>
+        List of Participants
+    </h3>
+</h2>
+<h2>
+    List of Archived Events
+</h2>
+<?php
+include('../System/Footer.php');
+?>
+
