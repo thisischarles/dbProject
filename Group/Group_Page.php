bcfc62ef4db82c10abe9fbcf01862c8d1a1083e5
Jessica Allaire <jessica.allaire.96@hotmail.com>
2019-11-11 11:35:33 -0500

    Added group page

--- Group/Group_Page.php
+++ Group/Group_Page.php
@@ -1,1 +1,50 @@
 <?php
+include('../System/Header.php');
+?>
+<table>
+    <tr width="100%">
+        <td width="88%">
+            <h1>
+               Event Name
+            </h1>
+            <h2>
+                Group Name
+            </h2>
+            <h3>
+                Information
+            </h3>
+            <h3>
+                <button>Post</button>
+            </h3>
+            <h3>
+                Past Posts
+            </h3>
+        </td>
+        <td align="right">
+            <h4>
+                <table>
+                    <tr>
+                        <td> Participant 1 </td>
+                        <td> <a> Message </a> </td>
+                    </tr>
+                    <tr>
+                        <td> Participant 2 </td>
+                        <td> <a> Message </a> </td>
+                    </tr>
+                    <tr>
+                        <td> Participant 3 </td>
+                        <td> <a> Message </a> </td>
+                    </tr>
+                    <tr>
+                        <td> Participant 4 </td>
+                        <td> <a> Message </a> </td>
+                    </tr>
+                </table>
+            </h4>
+        </td>
+    </tr>
+</table>
+<?php
+include('../System/Footer.php');
+?>
+
