2439f92c5eca241012c3c2955ed0fb181d37e0b9
Jessica Allaire <jessica.allaire.96@hotmail.com>
2019-11-13 08:37:56 -0500

    Put the JS in a different file, adding the button stuff into design
    Also fixed popups for marios stuff

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="ControllerCSS.css"/>
-<title>Controller</title>
+    <link rel="stylesheet" href="../design.css" />
+    <title>Controller</title>
 </head>
 <body>
 <?php
@@ -29,7 +29,7 @@ Controller
 <button class="button" onClick="openPeriodForm()">Set period</button>
 <br>
 <br>
-<div class="setchargerateform-popup" id="ChargeRateForm">
+<div class="form-popup" id="ChargeRateForm">
 	<form action="/action_page.php" Class="form-container">
 		<h1>Set charge rate</h1>
 		<br>
@@ -37,10 +37,10 @@ Controller
 		<input type="text" placeholder="Enter the Charge rate" name="ChargeRate" required>
 		<br>
 		<button type="submit" class="submitButton">Submit</button>
-		<button type="submit" class="cancelButton" onClick="closeChargeRateForm()">Cancel</button>
+		<button type="submit" class="closeButton" onClick="closeChargeRateForm()">Cancel</button>
 	</form>
 </div>
-<div class="setbandwidthform-popup" id="BandWidthForm">
+<div class="form-popup" id="BandWidthForm">
 	<form action="/action_page.php" Class="form-container">
 		<h1>Set bandwidth</h1>
 		<br>
@@ -48,10 +48,10 @@ Controller
 		<input type="text" placeholder="Enter the Bandwidth" name="Bandwidth" required>
 		<br>
 		<button type="submit" class="submitButton">Submit</button>
-		<button type="submit" class="cancelButton" onClick="closeBandWidthForm()">Cancel</button>
+		<button type="submit" class="closeButton" onClick="closeBandWidthForm()">Cancel</button>
 	</form>
 </div>
-<div class="setdiscountform-popup" id="DiscountForm">
+<div class="form-popup" id="DiscountForm">
 	<form action="/action_page.php" Class="form-container">
 		<h1>Set discount</h1>
 		<br>
@@ -59,10 +59,10 @@ Controller
 		<input type="text" placeholder="Enter the Discount" name="Discount" required>
 		<br>
 		<button type="submit" class="submitButton">Submit</button>
-		<button type="submit" class="cancelButton" onClick="closeDiscountForm()">Cancel</button>
+		<button type="submit" class="closeButton" onClick="closeDiscountForm()">Cancel</button>
 	</form>
 </div> 
-<div class="setstorageform-popup" id="StorageForm">
+<div class="form-popup" id="StorageForm">
 	<form action="/action_page.php" Class="form-container">
 		<h1>Set Storage</h1>
 		<br>
@@ -70,63 +70,22 @@ Controller
 		<input type="text" placeholder="Enter the Storage capacity" name="Storage" required>
 		<br>
 		<button type="submit" class="submitButton">Submit</button>
-		<button type="submit" class="cancelButton" onClick="closeEventStatusForm()">Cancel</button>
+		<button type="submit" class="closeButton" onClick="closeEventStatusForm()">Cancel</button>
 	</form>
 </div>
-<div class="setperiodform-popup" id="PeriodForm">
+<div class="form-popup" id="PeriodForm">
 	<form action="/action_page.php" Class="form-container">
 		<h1>Set Period</h1>
 		<br>
 		<label for="Period"><b>Period</b></label>
 		<br>
 		<button type="submit" class="submitButton">Submit</button>
-		<button type="submit" class="cancelButton" onClick="closeListOfEventsForm()">Cancel</button>
+		<button type="submit" class="closeButton" onClick="closeListOfEventsForm()">Cancel</button>
 	</form>
 </div>
 <?php
 include('../System/Footer.php');
 ?>
-<script>
-function openChargeRateForm()
-{
-	document.getElementById("ChargeRateForm").style.display="block";
-}
-function openBandWidthForm()
-{
-	document.getElementById("BandWidthForm").style.display="block";
-}
-function openDiscountForm()
-{
-	document.getElementById("DiscountForm").style.display="block";
-}
-function openStorageForm()
-{
-	document.getElementById("StorageForm").style.display="block";
-}
-function openPeriodForm()
-{
-	document.getElementById("PeriodForm").style.display="block";
-}
-function closeChargeRateForm()
-{
-	document.getElementById("ChargeRateForm").style.display="none";
-}
-function closeBandWidthForm()
-{
-	document.getElementById("BandWidthForm").style.display="none";
-}
-function closeDiscountForm()
-{
-	document.getElementById("DiscountForm").style.display="none";
-}
-function closeStorageForm()
-{
-	document.getElementById("StorageForm").style.display="none";
-}
-function closePeriodForm()
-{
-	document.getElementById("PeriodForm").style.display="none";
-}
-</script>
+<script src="Controller.js"></script>
 </body>
 </html>
