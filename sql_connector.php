58402bfc114eacb53ae2f167a9cad99ad4604cfc
Jessica Allaire <jessica.allaire.96@hotmail.com>
2019-12-04 22:43:59 -0500

    SQL connector for each page

--- sql_connector.php
+++ sql_connector.php
@@ -1,1 +1,10 @@
+
 <?php
+/*
+$user = 'jrc353_2';
+$password = 'gfgaFP';
+$database = 'jrc353_2';
+$db = new mysqli("jrc353.encs.concordia.ca", $user, $password, $database) or die("cannot connect");
+*/
+?>
+
